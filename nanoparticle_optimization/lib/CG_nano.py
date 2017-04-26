3bc1df80c858a62f3b9a4216bdf4916ab9ce855f
Andrew Summers <andrew.z.summers@gmail.com>
2017-04-26 16:50:14 -0500

    CG_nano initialization is now much faster

--- nanoparticle_optimization/lib/CG_nano.py
+++ nanoparticle_optimization/lib/CG_nano.py
@@ -6,8 +6,23 @@ import mbuild as mb
 import numpy as np
 from scipy.spatial import distance
 
-# Coarse-grained nanoparticle class
+def _fast_sphere_pattern(n, radius):
+    """Faster version of mBuild's SpherePattern. """
+    phi = (1 + np.sqrt(5)) / 2
+    long_incr = 2*np.pi / phi
+    dz = 2.0 / float(n)
+    bands = np.arange(n)
+    z = bands * dz - 1.0 + (dz/2.0)
+    r = np.sqrt(1.0 - z*z)
+    az = bands * long_incr
+    x = r * np.cos(az)
+    y = r * np.sin(az)
+    points = np.column_stack((x, y, z)) * np.asarray([radius])
+
+    return points
+
 class CG_nano(mb.Compound):
+    """Coarse-grained nanoparticle class. """
     def __init__(self, r=5.0, sigma=0.8):
         super(CG_nano, self).__init__()
 
@@ -27,22 +42,19 @@ class CG_nano(mb.Compound):
         opt_points = 0
         while opt_points == 0:
             mid = math.ceil((max_points + min_points) / 2)
-            mask = mb.SpherePattern(mid)
-            mask.scale(r)
-            mask_high = mb.SpherePattern(mid + 1)
-            mask_high.scale(r)
-            check = self._check_overlap(mask, r_CG)
-            check_high = self._check_overlap(mask_high, r_CG)
+            points = _fast_sphere_pattern(mid, r)
+            points_high = _fast_sphere_pattern(mid+1, r)
+            check = self._check_overlap(points, r_CG)
+            check_high = self._check_overlap(points_high, r_CG)
             if check == False and check_high == True:
-                mask = mb.SpherePattern(mid)
-                mask.scale(r - r_CG + r_silica)
+                points = _fast_sphere_pattern(mid, r - r_CG + r_silica)
                 opt_points = mid
             elif check == True:
                 max_points = mid - 1
             else:
                 min_points = mid + 1
 
-        for i, pos in enumerate(mask):
+        for i, pos in enumerate(points):
             particle = mb.Compound(name="CG", pos=pos)
             self.add(particle, "CG_{}".format(i))
 
@@ -60,5 +72,5 @@ class CG_nano(mb.Compound):
 
 
 if __name__ == "__main__":
-    cg_nano = CG_nano(r=3.0, sigma=0.9)
-    cg_nano.save('test.mol2')
+    cg_nano = CG_nano(r=8.0, sigma=0.6)
+    cg_nano.save('test-fast.mol2', overwrite=True)
