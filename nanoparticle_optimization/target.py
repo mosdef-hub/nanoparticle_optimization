ce8f65a315aa2941ba69570fae2f7707051b0500
Andrew Z. Summers <andrew.z.summers@gmail.com>
2017-07-21 13:11:44 -0500

    Read error from target data file

--- nanoparticle_optimization/target.py
+++ nanoparticle_optimization/target.py
@@ -2,14 +2,22 @@ import numpy as np
 
 def load(filename):
     target_data = np.loadtxt(filename)
-    return Target(target_data[:,0], target_data[:,1])
+    try:
+        target = Target(target_data[:,0], target_data[:,1], target_data[:,2])
+    except IndexError:
+        target = Target(target_data[:,0], target_data[:,1])
+    return target
 
 class Target(object):
-    def __init__(self, separations, potential):
+    def __init__(self, separations, potential, error=None):
         super(Target, self).__init__()
 
         self.separations = separations
         self.potential = potential
+        if error is None:
+            self.error = np.zeros(potential.shape[0])
+        else:
+            self.error = error
 
 if __name__ == "__main__":
     pass
