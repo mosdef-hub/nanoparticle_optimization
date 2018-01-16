0935bf621692d128a6103b06a3e3992ccd3e6c9a
Andrew Z. Summers <andrew.z.summers@gmail.com>
2018-01-16 00:29:18 -0600

    Added new NP-NP optimization

--- nanoparticle_optimization/results/np-np/opt3/brute/5nm/optimize-1.0.py
+++ nanoparticle_optimization/results/np-np/opt3/brute/5nm/optimize-1.0.py
@@ -1,8 +1,7 @@
 from __future__ import division
 
 import dill
-import os
-from pkg_resources import resource_filename
+import pkg_resources
 import sys
 
 import numpy as np
@@ -15,17 +14,17 @@ import nanoparticle_optimization as np_opt
 Statepoint
 ----------
 '''
-sigma_bead = 0.8
-radius = 6.0
+sigma_bead = 1.0
+target_radius = 5.0
 
 '''
 --------------------
 Optimization Details
 --------------------
 '''
-gridpoints = 10
-configurations = 10
-r_dependent_sampling = True
+gridpoints = 8
+configurations = 25
+r_dependent_sampling = False
 
 '''
 ----------------
@@ -33,9 +32,9 @@ Load Target Data
 ----------------
 '''
 resource_package = np_opt.__name__
-target_path = os.path.join('utils', 'target_data', 'np_np', 'truncated',
-    'U_{}nm_truncated.txt'.format(int(radius)))
-target = np_opt.load_target(resource_filename(resource_package, target_path))
+resource_path = '/'.join(('utils', 'U_{}nm_truncated.txt'.format(int(radius))))
+target = np_opt.load_target(pkg_resources.resource_filename(resource_package,
+                                                            resource_path))
 target.separations /= 10 # Convert distances from angstroms to nanometers
 
 '''
@@ -52,9 +51,9 @@ Define Force field Parameters and Bounds
 ----------------------------------------
 '''
 sigma = np_opt.Parameter(value=sigma_bead, fixed=True)
-m = np_opt.Parameter(value=6.0, upper=7.0, lower=4.0)
-epsilon = np_opt.Parameter(value=2.0, upper=6.0, lower=0.5)
-n = np_opt.Parameter(value=20.0, upper=50.0, lower=12.0)
+m = np_opt.Parameter(value=6.0, upper=7.0, lower=5.0)
+epsilon = np_opt.Parameter(value=2.0, upper=4.0, lower=0.5)
+n = np_opt.Parameter(value=20.0, upper=45.0, lower=12.0)
 forcefield = np_opt.Mie(sigma=sigma, epsilon=epsilon, n=n, m=m)
 
 '''
