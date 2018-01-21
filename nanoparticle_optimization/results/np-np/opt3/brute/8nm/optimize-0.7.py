4b0f998fc29bb118f429728cfd5a058c1424fb42
Andrew Z. Summers <andrew.z.summers@gmail.com>
2018-01-21 13:48:17 -0600

    Added additional sigmas for NP-NP optimization 3

---
+++ nanoparticle_optimization/results/np-np/opt3/brute/8nm/optimize-0.7.py
@@ -0,0 +1,75 @@
+from __future__ import division
+
+import dill
+import os
+from pkg_resources import resource_filename
+import sys
+
+import numpy as np
+
+import nanoparticle_optimization as np_opt
+
+
+'''
+----------
+Statepoint
+----------
+'''
+sigma_bead = 0.7
+radius = 8.0
+
+'''
+--------------------
+Optimization Details
+--------------------
+'''
+gridpoints = 20
+configurations = 10
+r_dependent_sampling = True
+
+'''
+----------------
+Load Target Data
+----------------
+'''
+resource_package = np_opt.__name__
+target_path = os.path.join('utils', 'target_data', 'np_np', 'truncated',
+    'U_{}nm_truncated.txt'.format(int(radius)))
+target = np_opt.load_target(resource_filename(resource_package, target_path))
+target.separations /= 10 # Convert distances from angstroms to nanometers
+
+'''
+------------------------------
+Create Two-Nanoparticle System
+------------------------------
+'''
+nano = np_opt.CG_nano(radius, sigma=sigma_bead)
+system = np_opt.System(nano)
+
+'''
+----------------------------------------
+Define Force field Parameters and Bounds
+----------------------------------------
+'''
+sigma = np_opt.Parameter(value=sigma_bead, fixed=True)
+m = np_opt.Parameter(value=6.0, upper=7.0, lower=3.0)
+epsilon = np_opt.Parameter(value=2.0, upper=6.0, lower=0.1)
+n = np_opt.Parameter(value=20.0, upper=60.0, lower=12.0)
+forcefield = np_opt.Mie(sigma=sigma, epsilon=epsilon, n=n, m=m)
+
+'''
+-----------------------------------
+Define and Execute the Optimization
+-----------------------------------
+'''
+optimization = np_opt.Optimization(forcefield=forcefield, systems=system,
+                                   targets=target, configurations=configurations)
+optimization.optimize(gridpoints=gridpoints, verbose=True, polishing_function=None,
+                      r_dependent_sampling=r_dependent_sampling)
+
+'''
+--------------------------
+Serialize the Optimization
+--------------------------
+'''
+dill.dump(optimization, open('opt-sigma{}.p'.format(sigma_bead), 'wb'))
