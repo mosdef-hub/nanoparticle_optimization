bcfe0317c664e23d1bf64778af56b078d46f0fdf
Andrew Summers <andrew.z.summers@gmail.com>
2018-01-22 15:50:10 -0600

    Added optimization 4 for NP-NP parameters

--- nanoparticle_optimization/results/np-np/opt4/brute/5nm/optimize-0.6.py
+++ nanoparticle_optimization/results/np-np/opt4/brute/5nm/optimize-0.6.py
@@ -1,7 +1,8 @@
 from __future__ import division
 
 import dill
-import pkg_resources
+import os
+from pkg_resources import resource_filename
 import sys
 
 import numpy as np
@@ -15,16 +16,16 @@ Statepoint
 ----------
 '''
 sigma_bead = 0.6
-target_radius = 5.0
+radius = 5.0
 
 '''
 --------------------
 Optimization Details
 --------------------
 '''
-gridpoints = 8
-configurations = 25
-r_dependent_sampling = False
+gridpoints = 25
+configurations = 10
+r_dependent_sampling = True
 
 '''
 ----------------
@@ -32,9 +33,9 @@ Load Target Data
 ----------------
 '''
 resource_package = np_opt.__name__
-resource_path = '/'.join(('utils', 'U_{}nm_truncated.txt'.format(int(radius))))
-target = np_opt.load_target(pkg_resources.resource_filename(resource_package,
-                                                            resource_path))
+target_path = os.path.join('utils', 'target_data', 'np_np', 'truncated',
+    'U_{}nm_truncated.txt'.format(int(radius)))
+target = np_opt.load_target(resource_filename(resource_package, target_path))
 target.separations /= 10 # Convert distances from angstroms to nanometers
 
 '''
@@ -51,9 +52,9 @@ Define Force field Parameters and Bounds
 ----------------------------------------
 '''
 sigma = np_opt.Parameter(value=sigma_bead, fixed=True)
-m = np_opt.Parameter(value=6.0, upper=7.0, lower=5.0)
-epsilon = np_opt.Parameter(value=2.0, upper=4.0, lower=0.5)
-n = np_opt.Parameter(value=20.0, upper=45.0, lower=12.0)
+m = np_opt.Parameter(value=6.0, upper=7.0, lower=3.0)
+epsilon = np_opt.Parameter(value=2.0, upper=6.0, lower=0.1)
+n = np_opt.Parameter(value=35.0, fixed=True)
 forcefield = np_opt.Mie(sigma=sigma, epsilon=epsilon, n=n, m=m)
 
 '''
