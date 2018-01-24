2c43fa0b3fbc21cbb7419da2face3f77cabdee6a
Andrew Z. Summers <andrew.z.summers@gmail.com>
2018-01-24 09:52:10 -0600

    Added new optimization for NP-NP parameters

---
+++ nanoparticle_optimization/results/np-np/opt5/brute/7nm/optimize-1.1.py
@@ -0,0 +1,75 @@
+from __future__ import division
+
+import dill
+import os
+from pkg_resources import resource_filename
+import sys
+
+import numpy as np
+
+import nanoparticle_optimization as np_opt
+
+
+'''
+----------
+Statepoint
+----------
+'''
+sigma_bead = 1.1
+radius = 7.0
+
+'''
+--------------------
+Optimization Details
+--------------------
+'''
+gridpoints = 25
+configurations = 10
+r_dependent_sampling = True
+
+'''
+----------------
+Load Target Data
+----------------
+'''
+resource_package = np_opt.__name__
+target_path = os.path.join('utils', 'target_data', 'np_np', 'truncated',
+    'U_{}nm_truncated.txt'.format(int(radius)))
+target = np_opt.load_target(resource_filename(resource_package, target_path))
+target.separations /= 10 # Convert distances from angstroms to nanometers
+
+'''
+------------------------------
+Create Two-Nanoparticle System
+------------------------------
+'''
+nano = np_opt.CG_nano(radius, sigma=sigma_bead)
+system = np_opt.System(nano)
+
+'''
+----------------------------------------
+Define Force field Parameters and Bounds
+----------------------------------------
+'''
+sigma = np_opt.Parameter(value=sigma_bead, fixed=True)
+m = np_opt.Parameter(value=6.0, upper=7.0, lower=3.0)
+epsilon = np_opt.Parameter(value=2.0, upper=8.0, lower=0.01)
+n = np_opt.Parameter(value=35.0, fixed=True)
+forcefield = np_opt.Mie(sigma=sigma, epsilon=epsilon, n=n, m=m)
+
+'''
+-----------------------------------
+Define and Execute the Optimization
+-----------------------------------
+'''
+optimization = np_opt.Optimization(forcefield=forcefield, systems=system,
+                                   targets=target, configurations=configurations)
+optimization.optimize(gridpoints=gridpoints, verbose=True, polishing_function=None,
+                      r_dependent_sampling=r_dependent_sampling)
+
+'''
+--------------------------
+Serialize the Optimization
+--------------------------
+'''
+dill.dump(optimization, open('opt-sigma{}.p'.format(sigma_bead), 'wb'))
---
+++ nanoparticle_optimization/results/np-np/opt5/polishing/test-5nm.py
@@ -0,0 +1,37 @@
+from __future__ import division
+
+import numpy as np
+from scipy.optimize import curve_fit
+
+import nanoparticle_optimization as np_opt
+from nanoparticle_optimization.testing import test_25nm
+
+
+sigma = [0.4, 0.7, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]
+epsilon = [0.36192, 1.39136, 2.41813,
+           2.87120, 3.40631, 4.11224, 4.43503, 5.29938, 5.68701, 6.71327, 6.96366,
+           7.05099, 7.62263, 7.65790]
+m = [4.34007, 4.83336, 5.13034,
+     5.23675, 5.33795, 5.48890, 5.53134, 5.65830, 5.69174, 5.81852, 5.83916,
+     5.84729, 5.88147, 5.88346]
+
+linear_fit = lambda x, a, b: a * x + b
+e_popt, e_pcov = curve_fit(linear_fit, sigma, epsilon)
+
+log_fit = lambda x, a, b: a * np.log(x) + b
+m_popt, m_pcov = curve_fit(log_fit, sigma, m)
+
+n_test = [35, 40, 45, 50]
+
+forcefields = []
+for n in n_test:
+    epsilon = linear_fit(sigma, *e_popt)
+    m = log_fit(sigma, *m_popt)
+    sigma = 5.0
+    forcefield = np_opt.Mie(sigma=np_opt.Parameter(value=sigma, fixed=True),
+                            epsilon=np_opt.Parameter(value=epsilon, fixed=True),
+                            n=np_opt.Parameter(value=n, fixed=True),
+                            m=np_opt.Parameter(value=m, fixed=True))
+    forcefields.append(forcefield)
+
+np_opt.test_25nm(forcefields, tag='cgff5')
